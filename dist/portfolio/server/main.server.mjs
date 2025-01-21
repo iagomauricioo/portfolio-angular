7275c43a437e4e18e28c2b9b1a043ee208397373
iago <iagomauricio7@gmail.com>
2025-01-21 19:29:49 -0300

    responsivo

--- dist/portfolio/server/main.server.mjs
+++ dist/portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XSNDEWNH.mjs";import"./chunk-BPZXAMS2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7V7DYUQ4.mjs";import"./chunk-BPZXAMS2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
